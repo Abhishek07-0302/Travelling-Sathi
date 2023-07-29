728232232f3912c073eb7de45ccb2f8f93053c44
Abhishek <kr.abhishek0183@gmail.com>
2023-07-29 11:14:34 +0530

    Added ratings in the map

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import {Paper,Typography,useMediaQuery  } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
-import Rating from "@material-ui/lab";
+import Rating from "@material-ui/lab/Rating";
 
 import useStyles from "./style";
 
@@ -11,7 +11,7 @@ import useStyles from "./style";
 
 const Map=({setCoordinates,setBounds,coordinates,places}) =>{
     const classes= useStyles();
-    const isMobile = useMediaQuery("(min-width:600px)");
+    const isDesktop = useMediaQuery("(min-width:600px)");
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact
@@ -29,8 +29,32 @@ const Map=({setCoordinates,setBounds,coordinates,places}) =>{
                 }}
                 onChildClick={""}
             >
-                {places?.map((place)=>(
-                        
+                {places?.map((place,i)=>(
+                        <div
+                        className={classes.markerContainer}
+                        lat={Number(place.latitude)}
+                        lng={Number(place.longitude)}
+                        key={i}
+                        >
+                            {
+                                !isDesktop ? (
+                                    <LocationOnOutlinedIcon color="primary" fontSize="large" />
+                                ) : (
+                                    <Paper elevation={3} className={classes.paper}>
+                                        <Typography className={classes.typography} variant="subtitle2" gutterbottom>{place.name}</Typography>
+                                        <img
+                                            className={classes.pointer}
+                                            src={place.photo? place.photo.images.large.url:"https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"}
+                                            alt={place.name}
+                                    
+                                        />
+                                        <Rating size="small" value={Number(place.rating)}  readOnly />
+                                    </Paper>
+
+                                )
+                            }
+
+                        </div>
                 ))}
 
             </GoogleMapReact>
